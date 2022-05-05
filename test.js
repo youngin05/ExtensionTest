d14c6ade0788b27c7025d2360fff4c31b15c8cc7
youngin <001frtt@naver.com>
2022-05-06 04:54:53 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
+// We use class syntax to define our extension object
+// This isn't actually necessary, but it tends to look the best
 
-
-class PiExtension {
+class MyExtension {
   /**
    * Scratch will call this method *once* when the extension loads.
    * This method's job is to tell Scratch things like the extension's ID, name, and what blocks it supports.
@@ -11,32 +12,36 @@ class PiExtension {
       // It should never change!
       // If you choose to make an actual extension, please change this to something else.
       // Only the characters a-z and 0-9 can be used. No spaces or special characters.
-      id: 'piextension',
+      id: 'myextensionexample',
 
       // `name` is what the user sees in the toolbox
       // It can be changed without breaking projects.
-      name: 'Pi Extension',
+      name: 'Cool Extension',
 
       blocks: [
         {
           // `opcode` is the internal ID of the block
           // It should never change!
           // It corresponds to the class method with the same name.
-          opcode: 'pivar',
+          opcode: 'hello',
           blockType: Scratch.BlockType.REPORTER,
-          text: 'Pi'
+          text: 'Hello, world!'
         }
       ]
     };
   }
 
-  pivar() {
+  /**
+   * Corresponds to `opcode: 'hello'` above
+   */
+  hello() {
     // You can just return a value: any string, boolean, or number will work.
-    return '3.14159265359';
+    // If you have to perform an asynchronous action like a request, just return a Promise.
+    // The block will wait until the Promise resolves and return the resolved value.
+    return 'Hello, world!';
   }
-
 }
 
 // Call Scratch.extensions.register to register your extension
 // Make sure to register each extension exactly once
-Scratch.extensions.register(new PiExtension());
+Scratch.extensions.register(new MyExtension());
